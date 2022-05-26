022f28719878cf335082ca7891eb5e4c34579e07
gitnotv <97412340+gitnotv@users.noreply.github.com>
2022-05-26 18:53:30 +1000

    Add files via upload

---
+++ cache/ext.py
@@ -0,0 +1,34 @@
+import asyncio
+import aiosqlite as db
+
+
+
+# able to create instances of this with the preferred id
+class permcache():
+
+    def __init__(self, id : str):
+        self.id = id
+    
+    async def _add(self, val : str = None) -> None:
+        if val == None:
+            val = 1
+
+        # simple numeric cache program (uniqueid based)
+        conn = await db.connect("cache.db")
+        cur = await conn.cursor()
+        await cur.execute("SELECT numeric FROM cache WHERE uniqueid = ?", (self.id,))
+        req = await cur.fetchone()
+
+        if req == None:
+            await cur.execute("INSERT INTO cache(numeric,uniqueid) VALUES (?,?)", (val, self.id))
+            await conn.commit()
+            await conn.close()
+            return val
+        elif req != None:
+            req = req[0]
+            await cur.execute("UPDATE cache SET numeric = numeric + ? WHERE uniqueid = ?", (val, self.id))
+            await conn.commit()
+            await conn.close()
+            return int(req) + val
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='numeric_cache',
+version='0.1',
+description='creates a permanent cache',
+url='#',
+author='max',
+install_requires=['opencv-python'],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
